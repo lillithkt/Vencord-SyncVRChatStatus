a125a9e7bc6771597e70cc9dd12e954c55a1c782
Lillith Fox <imlvnaa@gmail.com>
2024-12-08 18:00:20 -0500

    whoops forgot author

--- totp.ts
+++ totp.ts
@@ -14,6 +14,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Original source: https://github.com/turistu/totp-in-javascript/blob/main/totp.js
+ * Author: turistu
  */
 
 
